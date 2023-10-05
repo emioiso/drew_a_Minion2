028ef83d1b520e589cb90b814cc53764525c38ee
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-05 17:49:09 +0900

    Initial Commit

---
+++ drew_a_Minion2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  drew_a_Minion2
+//
+//  Created by emi oiso on 2023/10/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ drew_a_Minion2/drew_a_Minion2App.swift
@@ -0,0 +1,17 @@
+//
+//  drew_a_Minion2App.swift
+//  drew_a_Minion2
+//
+//  Created by emi oiso on 2023/10/05.
+//
+
+import SwiftUI
+
+@main
+struct drew_a_Minion2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
